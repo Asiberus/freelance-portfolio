a16346b87431209ab59c76020832bc0729abfe13
Raphael Beekmann <raphael.beekmann@gmail.com>
2023-05-16 12:12:37 +0200

    feat: improve layout.vue
    add main.scss file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,53 @@
 <template>
-  <header>
-    <h1>Raphael BEEKMANN</h1>
-    <nav>
-      <NuxtLink to="/">Accueil</NuxtLink>
-      <NuxtLink to="/prestations">Prestations</NuxtLink>
-      <NuxtLink to="/whoami">Qui suis-je ?</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
-    </nav>
-  </header>
+    <header class="header">
+        <h1 class="header__title">Raphael BEEKMANN</h1>
+        <nav class="header__menu">
+            <template v-for="link of LINKS" :key="link.to">
+                <NuxtLink :to="link.to" class="header__menu__link" active-class="active">
+                    {{ link.name }}
+                </NuxtLink>
+            </template>
+        </nav>
+    </header>
 
-  <main>
-    <slot />
-  </main>
+    <main>
+        <slot />
+    </main>
 </template>
 
+<script setup lang="ts">
+const LINKS = [
+    { name: 'Accueil', to: '/' },
+    { name: 'Prestations', to: '/prestations' },
+    { name: 'Qui suis-je ?', to: '/whoami' },
+    { name: 'Contact', to: '/contact' },
+]
+</script>
+
 <style scoped lang="scss">
-a {
-  margin-right: 1rem;
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+
+    &__menu {
+        display: flex;
+        align-items: center;
+        column-gap: 2rem;
+
+        &__link {
+            color: grey;
+            transition: all 0.2s ease;
+
+            &:hover {
+                color: black;
+            }
+
+            &.active {
+                color: #222;
+            }
+        }
+    }
 }
 </style>
